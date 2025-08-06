0bec1a408c3254d7abf1e5897de6ca39b48ee28d
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2025-08-06 16:52:52 -0400

    Adding object that allow display last row

--- script.js
+++ script.js
@@ -121,13 +121,21 @@ for (let i = 0; i < cvsData.length; i++) {
 }
 
 // Display the last row
-if (currentCell !== '' || currentRow.length - 1) {
+if (currentCell !== '' || currentRow.length >0) {
 
   containerArrays.push([...currentRow]);
   console.log("----------------------Part 2--------------------");
   console.log(containerArrays);
   console.log("\n");
   console.log("----------------------Part 3--------------------")
-  console.log(newContainerObject);
-
+ // MISSING: Create object for last row too!
+    if (containerArrays.length > 1) { // If it's not just headers
+        let rowObject = {};
+        currentRow.forEach((value, index) => {
+            const key = headers[index].toLowerCase();
+            rowObject[key] = value;
+        });
+        newContainerObject.push(rowObject);
+    }
+    console.log(newContainerObject);
 }
