9d71b62c3dbd5bc04c27823f6abad62088151b3d
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2025-08-06 00:33:17 -0400

    Adding element for part 2 and part 3

--- script.js
+++ script.js
@@ -1,63 +1,133 @@
 
-
 const cvsData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26`;
-let cell1 = [];
-let cell2 = [];
-let cell3 = [];
-let rows = [];
+
+// let currentCellIndex = 0;
+// numberCols = 4;
+// ContainerRows = [];
+// let currentRow = [];
+// let currentCell = '';
+// firstRow = true;
+
+// for (let i = 0; i < cvsData.length; i++) {
+
+//   const char = cvsData[i];
+//   // console.log(`Current character: ${char}`);
+//   if (char === ',') {
+
+//     currentRow.push(currentCell);
+//     //reset the cell after populate the cell in row
+//     currentCell = '';
+
+//   }
+//   else if (char === '\n') {
+
+//     currentRow.push(currentCell);
+//     // console.log(currentRow);
+
+//     if (firstRow) {
+//       numCols = currentRow.length;
+//       // console.log(`number of ${numCols}`);
+//       firstRow = false;
+//     }
+
+//     ContainerRows.push([...currentRow]);
+//     // console.log(`row ${ContainerRows} :`,currentRow);
+
+//     // reset row & cell
+//     currentCell = '';
+//     currentRow = [];
+
+//   } else {
+//     currentCell += char;
+
+//   }
+
+// }
+
+// // Display the last row
+// if (currentCell !== '' || currentRow.length > 0) {
+
+//   // console.log( "table",currentRow);
+//   ContainerRows.push([...currentRow]);
+//   console.log(ContainerRows);
+// }
+
+// ------------------------Part 3--------------------
+
 let currentCellIndex = 0;
-numberCols = 8;
-let numCols = 0;
-// cvsDatas=[];
+let numberCols = 4;
+let containerArrays = [];
 let currentRow = [];
 let currentCell = '';
+let firstRow = true;
+let cellCounter = 0;
+let headers = [];
+let newContainerObject = [];
+
+
 
 for (let i = 0; i < cvsData.length; i++) {
 
-    const char = cvsData[i];
-    // console.log(`Current character: ${char}`);
-    if (char === ',') {
+  const char = cvsData[i];
+  // console.log(`Current character: ${char}`);
+  if (char === ',') {
 
-        currentRow.push(currentCell);
-        currentCell = '';
+    currentRow.push(currentCell);
+    currentCell = '';
+    cellCounter++;
 
+  }
+  else if (char === '\n') {
+
+    currentRow.push(currentCell);
+    // console.log(currentRow);
+
+    // ----------------------Part 2--------------------------
+    if (firstRow) {
+      numCols = currentRow.length;
+      // console.log(`number of ${numCols}`);
+      firstRow = false;
     }
-    else if (char === '\n') {
+    // ------------------------Part 3----------------------------
+    containerArrays.push([...currentRow]);
 
-        currentRow.push(currentCell);
-        console.log(currentRow);
-        // reset row an cell
-          currentCell = '';
-        currentRow = [];
-      
+    if (containerArrays.length === 1) {
+      // console.log("====",containerArrays.length)
+      // Store header row in a single array
+      headers = currentRow;
 
-        currentCellIndex = 0;
     } else {
-        currentCell += char;
-        
-        // if (currentCellIndex === 0) {
-        //     // cell1 += char;
-        //     cell1.push(char);
-        // }
-        // else if (currentCellIndex === 1) {
-        //     // cell2 += char;
-        //     cell2.push(char);
-        // }
-        // else if (currentCellIndex === 2) {
-        //     // cell3 += char;
-        //     cell3.push(char);
-        // }
-        // else {
-        //     cell4.push(char);
-        // }
+      // Create object from current row using headers
+      let rowObject = {};//object variable
+
+      currentRow.forEach((value, index) => {
+        // console.log("current row",currentRow);
+        const key = headers[index].toLowerCase();
+        rowObject[key] = value;
+        // console.log("object",rowObject);
+      });
+      newContainerObject.push(rowObject);
     }
-  
+    // reset row & cell
+    currentCell = '';
+    currentRow = [];
+
+  } else {
+    currentCell += char;
+
+  }
+
+
 }
 
 // Display the last row
-if (currentCell !== '' || currentRow.length > 0)  {
-    
-     console.log(currentRow);
+if (currentCell !== '' || currentRow.length - 1) {
+
+  containerArrays.push([...currentRow]);
+  console.log("----------------------Part 2--------------------");
+  console.log(containerArrays);
+  console.log("\n");
+  console.log("----------------------Part 3--------------------")
+  console.log(newContainerObject);
+
 }
-
-
